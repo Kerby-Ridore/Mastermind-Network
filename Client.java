ab62d691053f90895a1ba2927b6715c74c5f162c
RIDORÉ Kerby <130407384+Ridore@users.noreply.github.com>
2025-02-27 21:27:46 +0100

    Client version 3

--- Client.java
+++ Client.java
@@ -18,27 +18,55 @@ public class Client {
 
             while(true){
 
-                //Lecture de la combinaison à tester
-                System.out.print("Entrée votre combinaison à tester: ");
-                String message = reader.readLine().toUpperCase();
-                out.writeUTF(message); // Envoi de la combinaison à tester au serveur
+                try {
 
-                //reception du nombre de couleurs correctement et incorrectement positionnées
-                String response = in.readUTF().toUpperCase();
-                System.out.println("Réponse du serveur: " + response);
+                    //Lecture de la combinaison à tester
+                    System.out.print("Entrez une combinaison de 4 couleurs (B, G, O, R, W, Y) : ");
+                    String message = reader.readLine().toUpperCase();
 
-                //Vérification de la combinaison test
-                if (response.equals("4,0")) {
-                    System.out.println("Félicitations");
+                    //vérification de la validité de l'entrée
+                    if (!isValidCombination(message)) {
+                        System.out.println("Combinaison invalide, Veuillez entrer une combinaison de  4 couleurs parmi B, G, O, R, W, Y.");
+                        continue;
+                    }
+                    out.writeUTF(message); // Envoi de la combinaison à tester au serveur
+
+                    //reception du nombre de couleurs correctement et incorrectement positionnées
+                    String response = in.readUTF().toUpperCase();
+                    System.out.println("Réponse du serveur: " + response);
+
+                    //Vérification de la combinaison test
+                    if (response.equals("POSITION CORRECTE: 4, POSITION INCORRECTE: 0")) {
+                        System.out.println("Félicitations");
+                        break;
+                    }
+                }
+                catch(EOFException e){
+                    System.out.println("Le serveur a fermé la connexion");
                     break;
                 }
             }
+            in.close();
+            out.close();
             client.close();
         }
         catch(IOException e){
             e.printStackTrace();
         }
 
+    }
+
+    //Méthode pour vérifier la validité de l'entrée
+    private static boolean isValidCombination(String message) {
+        if (message.length() != 4) {
+            return false;
+        }
+        for (char c : message.toCharArray()) {
+            if ("BGORWY".indexOf(c) == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
